a473d57ea3cda4061ee6279d1d99cb46b9eb6209
Iristyle <Iristyle@github>
2014-09-20 07:54:17 -0700

    Update windows_feature.ps1 fact to run on x64

--- facts/windows_features.ps1
+++ facts/windows_features.ps1
@@ -1,11 +1,27 @@
-$features = @()
+$command = {
+  $features = @()
 
-try
-{
-  $features = Get-WindowsFeature |
-    ? { $_.Installed } |
-    Select -ExpandProperty Name
+  try
+  {
+    $features = Get-WindowsFeature |
+      ? { $_.Installed } |
+      Select -ExpandProperty Name
+  }
+  catch {}
+
+  "windows_features=$($features -join ',')"
 }
-catch {}
 
-"windows_features=$($features -join ',')"
+if ([IntPtr]::Size -eq 8)
+{
+  &$command
+  return
+}
+
+$ps = @(
+    "${Env:SystemRoot}\sysnative\WindowsPowershell\v1.0\powershell.exe",
+    'powershell.exe') |
+  ? { (Test-Path $_) -or (Get-Command $_ -ErrorAction SilentlyContinue) } |
+  Select -First 1
+
+&$ps -Command $command
