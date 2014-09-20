d3bca101831d7b12437dd5f8b45e07c682635c4f
Iristyle <Iristyle@github>
2014-09-20 15:27:39 -0700

    Ensure that when master is setup, owner is good

--- config/setup-master.sh
+++ config/setup-master.sh
@@ -1,2 +1,4 @@
 sudo cp -f -R /vagrant/modules/* /etc/puppetlabs/puppet/modules
+sudo chown -R root:pe-puppet /etc/puppetlabs/puppet/modules
 sudo cp -f -R /vagrant/manifests/* /etc/puppetlabs/puppet/manifests
+sudo chown -R root:pe-puppet /etc/puppetlabs/puppet/manifests
